9a6f2a36b5a62ebec654c23d2da844a6178de9c9
Cole Moore <Cartisim@icloud.com>
2021-05-24 07:29:33 -0700

    restored files

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "cartisim-nio-client",
-    platforms: [
-        .macOS(.v11),
-        .iOS(.v14),
-        .tvOS(.v14),
-        .watchOS(.v7)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(name: "CartisimNIOClient", targets: ["CartisimNIOClient"]),
@@ -19,15 +13,21 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.27.0"),
         .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.8.0"),
         .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.9.2"),
+        .package(url: "https://github.com/apple/swift-nio-ssl.git", from: "2.7.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "0.0.5"))
     ],
     targets: [
         .target(
             name: "CartisimNIOClient",
             dependencies: [
                 .product(name: "NIOExtras", package: "swift-nio-extras"),
-                .product(name: "NIOTransportServices", package: "swift-nio-transport-services")
+                .product(name: "NIOTransportServices", package: "swift-nio-transport-services"),
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "NIOSSL", package: "swift-nio-ssl"),
+                .product(name: "NIOHTTP1", package: "swift-nio")
             ]
         ),
         .testTarget(name: "CartisimNIOClientTests", dependencies: ["CartisimNIOClient"]),
